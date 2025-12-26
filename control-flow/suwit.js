9c8c55c8a58ba454995f3da83abda13bd9d1e6f9
shidqisep <shidathalah@gmail.com>
2025-12-26 08:52:25 +0700

    suwit done

--- control-flow/suwit.js
+++ control-flow/suwit.js
@@ -9,8 +9,50 @@ playerChooseContainer.addEventListener('click', (playerChoose) => {
 
     const userChoice = buttonChoose.dataset.choice;
     console.log("user memilih " + userChoice)
-    // playSuwit(buttonChoose)
+    playSuwit(userChoice)
 })
+//logika permainan
+function playSuwit(userChoice){
+    let angkaAcak = Math.round(Math.random() * 2 + 1)
+// console.log(angkaAcak)
+    let compChoice
+
+    switch (angkaAcak) {
+        case 1:
+            compChoice = "Rock"
+            break;
+
+        case 2:
+            compChoice = "Scissor"
+            break;
+
+        default:
+            compChoice = "Paper"
+            break;
+    }
+
+    if (userChoice == compChoice) {
+        alert("Lu seri coy")
+    } else if(userChoice == "Paper"){
+        if (compChoice == "Rock") {
+            alert("anda Menang")
+        } else {
+            alert("anda kalah")
+        }
+    } else if (userChoice == "Rock") {
+        if (compChoice == "Scissor") {
+            alert("anda menang")
+        } else {
+            alert("Anda kalah")
+        }
+    } else if (userChoice == "Scissor") {
+        if (compChoice == "Paper") {
+            alert("anda menang")
+        }else{
+            alert("anda kalah")
+        }
+    }
+}
 
 const btnKembali = document.getElementById('btn-kembali')
 
